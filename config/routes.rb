7125eb1606eac263be57832b02a067c2dd514392
aldihidayat02 <aldihidayatt2005@gmail.com>
2022-10-17 16:09:45 +0700

    study

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  
+  # GET /about
+  get "/about", to: "about#index"
+
+  root to: "main#index"
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+end
